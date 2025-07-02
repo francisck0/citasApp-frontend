f7cbf58ef55d95b723d407f018b186ca2bb0bdd8
Fran J. Cano <francisco.canofuentes@gmail.com>
2025-07-02 23:43:54 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,50 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'citasapp-front';
+  protected searchQuery = signal('');
+  protected selectedSpecialty = signal('');
+  protected selectedLocation = signal('');
+  
+  protected specialties = signal([
+    'Medicina General', 'Cardiología', 'Dermatología', 'Ginecología',
+    'Pediatría', 'Traumatología', 'Oftalmología', 'Psicología',
+    'Dentista', 'Fisioterapia', 'Endocrinología', 'Neurología'
+  ]);
+
+  protected cities = signal([
+    'Madrid', 'Barcelona', 'Valencia', 'Sevilla',
+    'Zaragoza', 'Málaga', 'Murcia', 'Palma',
+    'Las Palmas', 'Bilbao', 'Alicante', 'Córdoba'
+  ]);
+
+  onSearch(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.searchQuery.set(target.value);
+  }
+
+  onSpecialtySelect(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedSpecialty.set(target.value);
+  }
+
+  onLocationSelect(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedLocation.set(target.value);
+  }
+
+  onSearchSubmit() {
+    console.log('Búsqueda:', {
+      query: this.searchQuery(),
+      specialty: this.selectedSpecialty(),
+      location: this.selectedLocation()
+    });
+  }
+
+  scrollToSection(sectionId: string, event: Event) {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
